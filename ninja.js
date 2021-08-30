b7c8d5d07b442d0b728fcb1c536f20c36b7c9cf1
Felix Almejo <fd.almejo@gmail.com>
2021-08-30 14:07:35 -0700

    Initial commit, working with classes - MERN

---
+++ ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health, speed =3,strength =3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    sayName() {
+        console.log(`Hello my name is, ${ this.name }!!`);
+    }
+    // simple method in the parent class
+    showStats(){
+        console.log(`Name: ${ this.name }, Strength: ${this.strength},Speed: ${this.speed},Health: ${this.health}`);
+    }
+    drinkSake(){
+        this.health+=10;
+    }
+}
+
+let felix = new Ninja('Felix',100)
+
+felix.sayName();
+felix.showStats();
+felix.drinkSake();
+felix.showStats();
